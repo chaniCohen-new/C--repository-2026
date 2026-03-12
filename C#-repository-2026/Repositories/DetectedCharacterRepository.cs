a2c9481d3549496ae46f6b2e2cbc682da09e6038
chani cohen <yaelc5570@gmail.com>
2026-03-12 16:19:54 +0200

    image and detectedCharacter repository

---
+++ C#-repository-2026/Repositories/DetectedCharacterRepository.cs
@@ -0,0 +1,86 @@
+﻿using C__repository_2026.Interfaces;
+using C__repository_2026.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Interfaces;
+
+namespace C__repository_2026.Repositories
+{
+    public class DetectedCharacterRepository : IRepository<DetectedCharacter>
+    {
+        private readonly IContext context;
+
+        public DetectedCharacterRepository(IContext context)
+        {
+            this.context = context;
+        }
+
+        public DetectedCharacter AddItem(DetectedCharacter item)
+        {
+            item.DetectionDate = DateTime.Now;
+            context.DetectedCharacters.Add(item);
+            context.Save();
+            return item;
+        }
+
+        public void DeleteItem(int id)
+        {
+            var detection = Get(id);
+            if (detection != null)
+            {
+                context.DetectedCharacters.Remove(detection);
+                context.Save();
+            }
+        }
+
+        public DetectedCharacter Get(int id)
+        {
+            return context.DetectedCharacters
+                .Where(dc => dc.Id == id)
+                .FirstOrDefault();
+        }
+
+        public List<DetectedCharacter> GetAll()
+        {
+            return context.DetectedCharacters
+                .OrderByDescending(dc => dc.DetectionDate)
+                .ToList();
+        }
+
+        public void UpdateItem(int id, DetectedCharacter item)
+        {
+            var detection = Get(id);
+            if (detection != null)
+            {
+                detection.Confidence = item.Confidence;
+                context.DetectedCharacters.Update(detection);
+                context.Save();
+            }
+        }
+
+        /// קבל את כל הדמויות שזוהו בתמונה מסוימת
+        /// מסודרות לפי רמת ודאות (הדיוק הגבוה ביותר קודם)
+        /// דוגמה: בתמונה X זוהו: Brad Pitt (95%), Tom Hanks (87%)
+        /// OrderByDescending: מסדר מהודאות הגבוהה לנמוכה
+        public List<DetectedCharacter> GetDetectionsByImageId(int imageId)
+        {
+            return context.DetectedCharacters
+                .Where(dc => dc.ImageId == imageId)
+                .OrderByDescending(dc => dc.Confidence)
+                .ToList();
+        }
+
+        /// קבל את כל התמונות שזוהתה בהן דמות מסוימת
+        /// מסודרות לפי תאריך זיהוי
+        public List<DetectedCharacter> GetDetectionsByCharacterId(int characterId)
+        {
+            return context.DetectedCharacters
+                .Where(dc => dc.CharacterId == characterId)
+                .OrderByDescending(dc => dc.DetectionDate)
+                .ToList();
+        }
+    }
+}
---
+++ C#-repository-2026/Repositories/ImageRepository.cs
@@ -0,0 +1,85 @@
+﻿using C__repository_2026.Interfaces;
+using C__repository_2026.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Interfaces;
+
+namespace C__repository_2026.Repositories
+{
+    public class ImageRepository : IRepository<Image>
+    {
+        private readonly IContext context;
+
+        public ImageRepository(IContext context)
+        {
+            this.context = context;
+        }
+
+        public Image AddItem(Image item)
+        {
+            context.Images.Add(item);
+            context.Save();
+            return item;
+        }
+
+        public void DeleteItem(int id)
+        {
+            var image = Get(id);
+            if (image != null)
+            {
+                context.Images.Remove(image);
+                context.Save();
+            }
+        }
+
+        public Image Get(int id)
+        {
+            return context.Images
+                .Where(i => i.Id == id)
+                .FirstOrDefault();
+        }
+
+        public List<Image> GetAll()
+        {
+            return context.Images.ToList();
+        }
+
+        public void UpdateItem(int id, Image item)
+        {
+            var image = Get(id);
+            if (image != null)
+            {
+                image.Url = item.Url;
+                image.GalleryId = item.GalleryId;
+                context.Images.Update(image);
+                context.Save();
+            }
+        }
+
+        public List<Image> GetImagesByGalleryId(int galleryId)
+        {
+            return context.Images
+                .Where(i => i.GalleryId == galleryId)
+                .ToList();
+        }
+
+        public List<Image> GetImagesByUserId(int userId)
+        {
+            return context.Images
+                .Where(i => i.UserId == userId)
+                .ToList();
+        }
+
+        /// קבל תמונה עם כל הדמויות שזוהו בה
+        /// זה חשוב כשרוצים להציג את התמונה ואת כל הדמויות שנמצאו בה
+        public Image GetImageWithDetections(int imageId)
+        {
+            return context.Images
+                .Where(i => i.Id == imageId)
+                .FirstOrDefault();
+        }
+    }
+}
