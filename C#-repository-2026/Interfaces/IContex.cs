f68ee58ee92ec073a635fe21c8fed01770090f15
chani cohen <yaelc5570@gmail.com>
2026-03-12 15:20:13 +0200

    add chracterRepository

---
+++ C#-repository-2026/Interfaces/IContex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using C__repository_2026.Entities;
+
+namespace C__repository_2026.Interfaces
+{
+    public interface IContext
+    {
+        public DbSet<Gallery> Galleries { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<DetectedCharacter> DetectedCharacters { get; set; }
+
+        public void Save();
+    }
+}
