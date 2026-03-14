10c87e460b4034f82c12e265693d611529f86d8e
yeudit <y00556735623@gmail.com>
2026-03-14 22:20:57 +0200

    Add IContext interface and update entities

---
+++ C#-repository-2026/Interfaces/IContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__repository_2026.Interfaces
+{
+    internal class IContext
+    {
+    }
+}
