51113ab6fa9afebca6108a2c968f160eac88500f
yeudit <y00556735623@gmail.com>
2026-03-16 16:03:41 +0200

    change the Entitis

--- C#-repository-2026/Entities/Gallery.cs
+++ C#-repository-2026/Entities/Gallery.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C__repository_2026.Entities
 {
     public class Gallery
     {
         public int Id { get; set; }
-        public string Name { get; set; }           // שם האוסף (למשל: "Brad Pitt")
+        public string Name { get; set; }           // שם האוסף (למשל: "דמות מסוימת")
         public int CharacterId { get; set; }       // איזו דמות זה
         public Character Character { get; set; }   // הקשר ל-Character
         public List<Image> Images { get; set; }    // כל התמונות באוסף הזה
--- C#-repository-2026/Entities/Image.cs
+++ C#-repository-2026/Entities/Image.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C__repository_2026.Entities
 {
     public class Image
     {
-public int Id { get; set; }
+        public int Id { get; set; }
         public string Url { get; set; }
         public int GalleryId { get; set; } // קישור לגלריה
         public Gallery Gallery { get; set; }
         public List<DetectedCharacter> DetectedCharacters { get; set; } = new List<DetectedCharacter>();
         public int UserId { get; set; }
-}
+    }
 }
